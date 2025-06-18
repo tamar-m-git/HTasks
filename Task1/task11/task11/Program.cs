571ce61c0ed1e8701997a93970a36724d48e2ffe
tamar <tamar0583252862@gmail.com>
2025-06-18 13:59:46 +0300

    Add project files.

---
+++ Task1/task11/task11/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+
+namespace task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N = 3;
+            var path = @"C:\training\Hadasim\HomeTask\TMPFiles\logs1.txt";
+            var dic=   CommonError(path, N);
+            Console.WriteLine("All Error:");
+            foreach (var kvp in dic.OrderByDescending(e => e.Value))
+            {
+                Console.WriteLine($"Error: {kvp.Key} - amount: {kvp.Value}");
+            }
+            Console.ReadLine();
+
+
+
+        }
+        public static Dictionary<string, int> CommonError(string FilePath, int N)
+        {
+            int NumThreads = Environment.ProcessorCount;
+            var outputDirectory = @"C:\training\Hadasim\HomeTask\FileOfThreads";
+            SplitFile(FilePath, NumThreads, outputDirectory);
+            List<int> NumberFiles = Enumerable.Range(0, 16).ToList();
+            var TotalErrors = new ConcurrentDictionary<string, int>();
+            Parallel.ForEach(NumberFiles, i =>
+            {
+                string filePath = Path.Combine(outputDirectory, $"Log{i}.txt");
+                if (File.Exists(filePath))
+                {
+                    Dictionary<string, int> ErrorCounts = CountError(filePath);
+                    //בדיקה אם זה עובד בצורה אסינכרונית- הדפסתי את מזהי התהליכונים
+                    foreach (var ObjDict in ErrorCounts)
+                    {
+                        TotalErrors.AddOrUpdate(
+                            ObjDict.Key,
+                            ObjDict.Value,
+                            (key, OldCount) => OldCount + ObjDict.Value
+                        );
+                    }
+
+                    Console.WriteLine($"Processing number: {i}");
+                }
+                else
+                    Console.WriteLine($"File Log{i}.txt not found");
+
+            });
+
+            var TopErr = TotalErrors
+            .OrderByDescending(x => x.Value)
+            .Take(N)
+            .ToDictionary(x => x.Key, x => x.Value);
+
+            return TopErr;
+               
+        }
+        public static void SplitFile(string LogfilePath, int NumSplit, string DirCreateFiles)
+
+        {
+            int lineCount = File.ReadLines(LogfilePath).Count();
+            int NumExtraLines = lineCount % NumSplit;
+
+            if (!Directory.Exists(DirCreateFiles))
+            {
+                Directory.CreateDirectory(DirCreateFiles);
+
+            }
+            using (StreamReader reader = new StreamReader(LogfilePath))
+            {
+
+                for (int i = 0; i < NumSplit; i++)
+                {
+                    string FileName = $"Log{i}.txt";
+                    string filePath = Path.Combine(DirCreateFiles, FileName);
+                    using (StreamWriter writer = new StreamWriter(filePath))
+                    {
+                        for (int j = 0; j < lineCount / NumSplit; j++)
+                        {
+                            var  line = reader.ReadLine();
+                            writer.WriteLine(line);
+
+                        }
+                        if (NumExtraLines > 0)
+                        {
+                            var line = reader.ReadLine();
+                            writer.WriteLine(line);
+                            NumExtraLines--;
+                        }
+
+                    }
+
+
+                }
+
+            }
+        }
+        public static Dictionary<string, int> CountError(string filePath)
+        {
+            int lineCount = File.ReadLines(filePath).Count();
+            var dictError = new Dictionary<string, int>();
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                for (int i = 0; i < lineCount; i++)
+                {
+                    var line = reader.ReadLine();
+                    int index = line.IndexOf("Error");
+                    string Err = line.Substring(index + "Error: ".Length);
+                    if(dictError.ContainsKey(Err))
+                         dictError[Err]++;
+                   
+                    else
+                        dictError[Err]=1;
+                }
+               
+
+            }
+            return dictError;
+        }
+
+    }
+}
